b0ad0415170c4343a0e7695e800f18541e485796
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-05-20 09:35:20 +0200

    Aggregates for the win

---
+++ src/test/java/dev/aleixmorgadas/aggregates/AggregatesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aleixmorgadas.aggregates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
